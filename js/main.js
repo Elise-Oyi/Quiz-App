b6932e14ee26d85b9f39a632cb2f63ea8dcf8ad6
Elise-oyi <maryeliseangbo@gmail.com>
2023-01-04 21:48:59 +0100

    my first commit

---
+++ js/main.js
@@ -0,0 +1,679 @@
+// an array for the questions
+
+const questions = [
+
+    {
+        id: 1,
+        question: " How old is Adansi travels?",
+        answer:        "9",
+        options:[
+        "5",
+        "10",
+        "9",
+        "7"
+        ]     
+    },
+        
+        
+    {
+        id: 2,
+        question:  "Adansi travels is the national tour operator of the year?",
+        answer:       "2021",
+        options:[
+        "2022",
+        "2020",
+        "2019",
+        "2021"
+        ]      
+    },
+
+
+    {
+        id:  3,
+        question:"Adansi travels is in how many countries?",
+        answer: "2",
+        options:[
+        "1",
+        "3",
+        "2",
+        "5"
+        ]          
+    },    
+
+
+    {
+        id:  4,
+        question:"Who is the CEO of Adansi travels?",
+        answer: "Gideon Asare",
+        options:[
+        "Mike Annan",
+        "Davida Adiga",
+        "Alexis Nana Addo",
+        "Gideon Asare"
+        ]     
+    }, 
+
+        
+    {
+        id: 5,
+        question:"Which country can I find Burj Khalifa?",
+        answer: "UAE",
+        options:[
+        "UAE",
+        "Dubai",
+        "Qatar",
+        "Seychelles"
+        ]      
+
+    },    
+
+
+    {
+        id:  6,
+        question:"Where is adansi travels main location?",
+        answer: "Accra",
+        options:[
+        "Kumasi",
+        "Koforidua",
+        "Accra",
+        "Tarkoradi"
+        ]     
+
+    },
+
+
+    // {
+    //     id:  7,
+    //     question:"question7",
+    //     answer: "7option1",
+    //     options:[
+    //     "7option1",
+    //     "7option2",
+    //     "7option3",
+    //     "7option4"
+    //     ]      
+    // },    
+        
+
+
+    // {
+    //     id:  8,
+    //     question:"question8",
+    //     answer: "8option4",
+    //     options:[
+    //     "8option1",
+    //     "8option2",
+    //     "8option3",
+    //     "8option4"
+    //     ]      
+    // },
+
+
+    // {
+    //     id:  9,
+    //     question:"question9",
+    //     answer: "9option3",
+    //     options:[
+    //     "9option1",
+    //     "9option2",
+    //     "9option3",
+    //     "9option4"
+    //     ]      
+    // },    
+        
+
+    // {
+    //     id:  10,
+    //     question:"question10",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  11,
+    //     question:"question11",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  12,
+    //     question:"question12",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  13,
+    //     question:"question13",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  14,
+    //     question:"question14",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  15,
+    //     question:"question15",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  16,
+    //     question:"question16",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  17,
+    //     question:"question17",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  18,
+    //     question:"question18",
+    //     answer:  "option4",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  19,
+    //     question:"question19",
+    //     answer:  "option2",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  20,
+    //     question:"question20",
+    //     answer:  "option3",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  21,
+    //     question:"question21",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  22,
+    //     question:"question22",
+    //     answer:  "option2",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  23,
+    //     question:"question23",
+    //     answer:  "option4",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  24,
+    //     question:"question24",
+    //     answer:  "option3",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  25,
+    //     question:"question25",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  26,
+    //     question:"question26",
+    //     answer:  "option2",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  27,
+    //     question:"question27",
+    //     answer:  "option2",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  28,
+    //     question:"question28",
+    //     answer:  "option1",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  29,
+    //     question:"question29",
+    //     answer:  "option4",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // },
+
+
+    // {
+    //     id:  30,
+    //     question:"question30",
+    //     answer:  "option2",
+    //     options:[
+    //         "option1",
+    //         "option2",
+    //         "option3",
+    //         "option4"
+    //     ]       
+    // }
+]
+
+
+const startBtn = document.querySelector(".start-btn")
+const gameRules = document.querySelector(".game-rules")
+const rulesQuitBtn = document.querySelector(".rules-quit-btn")
+const continuetBtn = document.querySelector(".rules-start-btn")
+const quizContainer = document.querySelector(".quiz-container")
+const questionText = document.querySelector(".que-text")
+const optionList = document.querySelector(".option-list")
+const nextQuestionBtn = document.querySelector(".next-btn")
+const totalQuestion = document.querySelector(".total-que")
+const endScore = document.querySelector(".score-text")
+const resultContainer = document.querySelector(".result-container")
+const restartGame = document.querySelector(".restart")
+const quitGame = document.querySelector(".quit")
+const timeSec = document.querySelector(".timer-sec")
+// const options = document.querySelectorAll(".option")
+
+
+let counter;
+let timeGiven = 5
+const initialState = {
+    score: 0,
+    wrongAnswer: 0,
+    totalQuestion: 5,
+    questionLoaded: 0
+}
+
+
+
+startBtn.addEventListener("click", 
+()=>{
+   startBtn.classList.add("start-hide") 
+    gameRules.classList.add("game-rules-show")
+}
+)
+
+rulesQuitBtn.addEventListener("click",
+()=>{
+    startBtn.classList.remove("start-hide") 
+   gameRules.classList.remove("game-rules-show")
+}
+)
+
+// Game starts
+continuetBtn.addEventListener("click",
+()=>{
+    // startBtn.classList.remove("start-hide") 
+   gameRules.classList.remove("game-rules-show")
+   quizContainer.classList.add("quiz-container-show")
+
+   setTimer(timeGiven)
+}
+)
+
+restartGame.addEventListener("click",()=> {
+    quizContainer.classList.add("quiz-container-show")
+    resultContainer.classList.remove("result-container-show")
+    initialStage()
+    displayQuestions()
+    clearInterval(counter)
+    setTimer(timeGiven)
+})
+
+quitGame.addEventListener("click",()=> {
+    window.location.reload();
+})
+
+
+// next question btn
+nextQuestionBtn.addEventListener("click",questionTracker)
+
+// a function to track the number of questions loaded
+function questionTracker(){  
+   
+    if(initialState.questionLoaded < 4) {       
+        displayQuestions()
+        initialState.questionLoaded ++
+        totalQuestion.innerHTML = `<span> <p>${initialState.questionLoaded + 1}</p> of <p>${initialState.totalQuestion}</p> questions</span>`
+        clearInterval(counter)
+        
+        setTimer(timeGiven)
+     }
+     
+     else{
+        displayResult()
+        clearInterval(counter)
+     } 
+        
+}
+
+function initialStage(){
+    initialState.score = 0
+    initialState.wrongAnswer = 0
+    // initialState.totalQuestion = 5
+    initialState.questionLoaded = 0
+
+}
+
+
+
+
+
+
+// a function to generate a random question
+function generateRandomNumber(index) {
+    // generating random index
+    index = Math.floor(Math.random() * questions.length)
+
+    return index
+}
+
+
+//a function to display questions and options
+ function displayQuestions() {
+
+        // Displaying questions and answers
+        const x = generateRandomNumber()
+        // console.log(x);
+         questionText.innerHTML = `<span>${questions[x].question}</span>`  
+         
+         optionList.innerHTML = ' <div class="option">'+ questions[x].options[0]+ '</div>'
+        +' <div class="option">'+ questions[x].options[1]+ '</div>'
+        +' <div class="option">'+ questions[x].options[2]+ '</div>'
+        +' <div class="option">'+ questions[x].options[3]+ '</div>'
+         nextQuestionBtn.disabled = true
+
+    
+          //targeting each option 
+        const options = optionList.querySelectorAll(".option")
+        
+        let correctTag = `<div class="icon"><i class='bx bx-check' ></i></div>`
+        let incorrectTag = `<div class="icon"><i class='bx bx-x' ></i></div>`
+     
+      function optionSelected(){
+        
+            //checking for correct answers
+        options.forEach(function (option) {
+        
+            option.addEventListener("click", function (e){
+                clearInterval(counter)
+                
+                const userAnswer = e.currentTarget.innerHTML
+
+                if(userAnswer == questions[x].answer ){
+
+                    // increasing the score if answer is correct
+                  initialState.score ++
+                  option.insertAdjacentHTML("beforeend", correctTag)
+                  option.style.backgroundColor = "rgb(200, 245, 222)"
+                  nextQuestionBtn.disabled = false
+                }
+
+                else{
+                    // console.log("incorrect answer");
+                    option.insertAdjacentHTML("beforeend", incorrectTag)
+                    option.style.backgroundColor = "rgb(244, 205, 205)"
+                    nextQuestionBtn.disabled = false
+
+                    
+                    // displaying the correct answer if user selects wrong answer
+                    for(i=0; i < optionList.children.length; i++){
+
+                       if(optionList.children[i].innerHTML == questions[x].answer ){
+
+                        optionList.children[i].insertAdjacentHTML("beforeend", correctTag)
+                        optionList.children[i].style.backgroundColor = "rgb(200, 245, 222)"
+                        console.log("yesss");
+                            }
+                        }
+                 }
+                  
+
+
+                for(i=0; i < optionList.children.length; i++){
+    
+                    optionList.children[i].classList.add("disabled")
+                   
+                }
+               
+
+            })
+        });
+        }
+        
+        optionSelected()
+
+        console.log(questions[x].answer);     
+    }
+
+    
+displayQuestions()
+
+
+// displaying results
+function displayResult(){
+    resultContainer.classList.add("result-container-show")
+    quizContainer.classList.remove("quiz-container-show")
+    if(endScore < 3){
+    endScore.innerHTML = `<span>sorry you got 🙁 <p>${initialState.score}</p> out of <p>5</p></span>`
+   }
+   else{
+    endScore.innerHTML = `<span>yay!! you got only😄 <p>${initialState.score}</p> out of <p>5</p></span>`
+   }
+}
+
+
+
+function setTimer(time){
+    //
+  counter = setInterval(timer, 1000)
+
+   function timer(){
+    timeSec.textContent = time
+    time--
+
+    if(time <= 9){
+       let addZero = "0" + time
+       timeSec.textContent = addZero
+    }
+
+
+    if(time <= 0){
+        clearInterval(counter)
+
+        timeSec.textContent = "time out"
+        nextQuestionBtn.disabled = false
+
+        // timeSec.style.color = "red"
+        //    //targeting each option 
+    
+           
+        //    let correctTag = `<div class="icon"><i class='bx bx-check' ></i></div>`
+
+        //    for(i=0; i < optionList.children.length; i++){
+          
+
+        //   console.log(x);
+
+        //     if(optionList.children[i].innerHTML == questions[x].answer ){
+                
+        //      optionList.children[i].insertAdjacentHTML("beforeend", correctTag)
+        //      optionList.children[i].style.backgroundColor = "rgb(200, 245, 222)"
+        //      console.log("yesss");
+        //          }
+        //      }
+
+         for(i=0; i < optionList.children.length; i++){
+
+         optionList.children[i].classList.add("disabled")
+       
+
+
+    }
+      }
+       
+
+
+    
+
+   }
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/questions.js
@@ -0,0 +1,396 @@
+
+
+const questions = [
+
+    {
+        id: 1,
+        question: " question1",
+        answer:        "answer",
+        options:[
+        "option1",
+        "option2",
+        "option3",
+        "option4"
+        ]     
+    },
+        
+        
+    {
+        id: 2,
+        question:  "question2",
+        answer:       "answer",
+        options:[
+        "option1",
+        "option2",
+        "option3",
+        "option4"
+        ]      
+    },
+
+
+    {
+        id:  3,
+        question:"question3",
+        answer: "answer",
+        options:[
+        "option1",
+        "option2",
+        "option3",
+        "option4"
+        ]          
+    },    
+
+
+    {
+        id:  4,
+        question:"question4",
+        answer: "answer",
+        options:[
+        "option1",
+        "option2",
+        "option3",
+        "option4"
+        ]     
+    }, 
+
+        
+    {
+        id: 5,
+        question:"question5",
+        answer: "answer",
+        options:[
+        "option1",
+        "option2",
+        "option3",
+        "option4"
+        ]      
+
+    },    
+
+
+    {
+        id:  6,
+        question:"question6",
+        answer: "answer",
+        options:[
+        "option1",
+        "option2",
+        "option3",
+        "option4"
+        ]     
+
+    },
+
+
+    {
+        id:  7,
+        question:"question7",
+        answer: "answer",
+        options:[
+        "option1",
+        "option2",
+        "option3",
+        "option4"
+        ]      
+    },    
+        
+
+
+    {
+        id:  8,
+        question:"question8",
+        answer: "answer",
+        options:[
+        "option1",
+        "option2",
+        "option3",
+        "option4"
+        ]      
+    },
+
+
+    {
+        id:  9,
+        question:"question9",
+        answer: "answer",
+        options:[
+        "option1",
+        "option2",
+        "option3",
+        "option4"
+        ]      
+    },    
+        
+
+    {
+        id:  10,
+        question:"question10",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  11,
+        question:"question11",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  12,
+        question:"question12",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  13,
+        question:"question13",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  14,
+        question:"question14",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  15,
+        question:"question15",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  16,
+        question:"question16",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  17,
+        question:"question17",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  18,
+        question:"question18",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  19,
+        question:"question19",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  20,
+        question:"question20",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  21,
+        question:"question21",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  22,
+        question:"question22",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  23,
+        question:"question23",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  24,
+        question:"question24",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  25,
+        question:"question25",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  26,
+        question:"question26",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  27,
+        question:"question27",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  28,
+        question:"question28",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  29,
+        question:"question29",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    },
+
+
+    {
+        id:  30,
+        question:"question30",
+        answer: "answer",
+        options:[
+            "option1",
+            "option2",
+            "option3",
+            "option4"
+        ]       
+    }
+]
